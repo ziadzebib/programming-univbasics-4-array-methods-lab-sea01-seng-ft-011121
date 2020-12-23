e893e1a8270910406817e72a97e2e0d14bccc57e
Ziad Zebib <zebibziad@gmail.com>
2020-12-23 19:45:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  array.sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse(array)
 end
 
 def using_first(array)
-
+  array.first(array)
 end
 
 def using_last(array)
-
+  array.last(array)
 end
 
 def using_size(array)
-
+  array.size(array)
 end
